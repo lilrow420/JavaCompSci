d2b0f01b1c8cc819867375741a204d475746800c
lilrow420 <rowsoto@gmail.com>
2019-01-16 14:41:29 -0600

    Add files via upload

---
+++ RowlandSotoMorseCodeRunner.java
@@ -0,0 +1,36 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Rowland Soto
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class RowlandSotoMorseCodeRunner
+{
+ public static void main( String args[] )
+ {
+  MorseCode test = new MorseCode('A');
+  out.println (test);
+
+  test = new MorseCode('B');
+  out.println (test);
+
+  test = new MorseCode('3');
+  out.println (test);
+
+  test = new MorseCode('Z');
+  out.println (test);
+
+  test = new MorseCode('8');
+  out.println (test);
+
+  test = new MorseCode('F');
+  out.println (test);
+
+  test = new MorseCode('0');
+  out.println (test);
+ }
+}
